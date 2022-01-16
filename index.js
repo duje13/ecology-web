5d5bbce5223a2cad28db5cf837a282d04b428c47
duje13 <dujesol@gmail.com>
2022-01-16 18:44:14 +0100

    Page structure

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
+const GRID_ROWS = 30;
+const GRID_COLUMNS = 15;
+const GRID_CELLS = GRID_COLUMNS * GRID_ROWS;
+
+/**
+ * Generate divs inside .grid div with 'type'
+ * class
+ */
+function generateGrids()
+{
+    let x = document.querySelectorAll(".grid");
+    for (let i = 0; i < x.length; i++) {
+        for (let j = 0; j < GRID_CELLS; j++) {
+            let el = document.createElement('div');
+            el.classList.add("type");
+            x[i].appendChild(el);
+        }
+    }
+}
+
+
+// SCRIPT BODY
 document.addEventListener('DOMContentLoaded', function () {
-    console.log("Hello!");
+    generateGrids();
 });
+
+/**
+ * Call on '.play-btn' click
+ */
+function onPlayClick()
+{
+    let btn = document.querySelector(".play-btn");
+    btn.classList.toggle("fa-play-circle");
+    btn.classList.toggle("fa-pause-circle");
+}
